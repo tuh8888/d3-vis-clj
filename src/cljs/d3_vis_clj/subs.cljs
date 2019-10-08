e3665445ea31a1bc8cc0244cf8ea0617aa3c524c
tuh8888 <harrison.pielke-lombardo@cuanschutz.edu>
2019-10-08 00:05:30 -0600

    Refer common fns

--- src/cljs/d3_vis_clj/subs.cljs
+++ src/cljs/d3_vis_clj/subs.cljs
@@ -1,131 +1,131 @@
 (ns d3-vis-clj.subs
   (:require-macros [reagent.ratom :refer [reaction]])
-  (:require [re-frame.core :as rf]))
+  (:require [re-frame.core :refer [subscribe reg-sub]]))
 
-(rf/reg-sub :name
+(reg-sub :name
   (fn [db]
     (:name db)))
 
-(rf/reg-sub :window-height
+(reg-sub :window-height
   (fn [db]
     (:height db)))
 
-(rf/reg-sub :window-width
+(reg-sub :window-width
   (fn [db]
     (:width db)))
 
-(rf/reg-sub :hierarchy
+(reg-sub :hierarchy
   (fn [db]
     (get-in db [:all-data :hierarchy])))
 
-(rf/reg-sub :force-layout
+(reg-sub :force-layout
   (fn [db [_ viz-id]]
     (get db viz-id)))
 
-(rf/reg-sub :node-size
+(reg-sub :node-size
   (fn [db [_ viz-id]]
     (get-in db [viz-id :node-config :r])))
 
-(rf/reg-sub :node-to-add
+(reg-sub :node-to-add
   (fn [db [_ viz-id]]
     (get-in db [viz-id :node-to-add])))
 
-(rf/reg-sub :link-config
+(reg-sub :link-config
   (fn [db [_ viz-id]]
     (get-in db [viz-id :link-config])))
 
-(rf/reg-sub :get-node
+(reg-sub :get-node
   (fn [db [_ viz-id i]]
     (get-in db [viz-id :data :nodes i])))
 
-(rf/reg-sub :node-color
+(reg-sub :node-color
   (fn [[_ viz-id i] _]
-    [(rf/subscribe [:hierarchy]) (rf/subscribe [:get-node viz-id i])])
+    [(subscribe [:hierarchy]) (subscribe [:get-node viz-id i])])
   (fn [[h {:keys [id hovered]}] _]
     (cond hovered "yellow"
           (isa? h id :A) "red"
           (isa? h id :B) "blue"
           :default "green")))
 
-(rf/reg-sub :node-name
+(reg-sub :node-name
   (fn [[_ viz-id i] _]
-    (rf/subscribe [:get-node viz-id i]))
+    (subscribe [:get-node viz-id i]))
   (fn [{:keys [name]} _]
     name))
 
-(rf/reg-sub :drag-fn
+(reg-sub :drag-fn
   (fn [db [_ viz-id]]
     (get-in db [viz-id :drag] #())))
 
-(rf/reg-sub :get-nodes
+(reg-sub :get-nodes
   (fn [db [_ viz-id]]
     (get-in db [viz-id :data :nodes])))
 
-(rf/reg-sub :get-nodes-js
+(reg-sub :get-nodes-js
   (fn [[_ viz-id] _]
-    (rf/subscribe [:get-nodes viz-id]))
+    (subscribe [:get-nodes viz-id]))
   (fn [data _]
     (clj->js data)))
 
-(rf/reg-sub :get-links
+(reg-sub :get-links
   (fn [db [_ viz-id]]
     (get-in db [viz-id :data :links])))
 
-(rf/reg-sub :get-links-js
+(reg-sub :get-links-js
   (fn [[_ viz-id] _]
-    (rf/subscribe [:get-links viz-id]))
+    (subscribe [:get-links viz-id]))
   (fn [data _]
     (clj->js data)))
 
-(rf/reg-sub :visible-mops
+(reg-sub :visible-mops
   (fn [db [_ viz-id]]
     (get-in db [viz-id :data])))
 
-(rf/reg-sub :selected-mops
+(reg-sub :selected-mops
   (fn [db [_ viz-id]]
     (get-in db [viz-id :selected])))
 
-(rf/reg-sub :mop-id
+(reg-sub :mop-id
   (fn [_ [_ mop]]
     (:id mop)))
 
-(rf/reg-sub :visible-roles
+(reg-sub :visible-roles
   (fn [db [_ viz-id]]
     (get-in db [viz-id :visible-roles])))
 
-(rf/reg-sub :reversed-col
+(reg-sub :reversed-col
   (fn [db [_ viz-id]]
     (get-in db [viz-id :reversed-col])))
 
-(rf/reg-sub :rev?
+(reg-sub :rev?
   (fn [[_ viz-id] _]
-    (rf/subscribe [:reversed-col viz-id]))
+    (subscribe [:reversed-col viz-id]))
   (fn [rev [_ _ col-key]]
     (= col-key rev)))
 
-(rf/reg-sub :selected-mop?
+(reg-sub :selected-mop?
   (fn [[_ viz-id] _]
-    (rf/subscribe [:selected-mops viz-id]))
+    (subscribe [:selected-mops viz-id]))
   (fn [selected-mops [_ _ id]]
     (contains? selected-mops id)))
 
-(rf/reg-sub :visible-role?
+(reg-sub :visible-role?
   (fn [[_ viz-id] _]
-    (rf/subscribe [:visible-roles viz-id]))
+    (subscribe [:visible-roles viz-id]))
   (fn [roles [_ _ role]]
     (some #(= role %) roles)))
 
-(rf/reg-sub :all-roles
+(reg-sub :all-roles
   (fn [[_ viz-id] _]
-    (rf/subscribe [:visible-mops viz-id]))
+    (subscribe [:visible-mops viz-id]))
   (fn [mops _]
     (->> mops
          (map :slots)
          (mapcat keys)
          (set))))
 
-(rf/reg-sub :all-roles-visible?
+(reg-sub :all-roles-visible?
   (fn [[_ viz-id] _]
-    [(rf/subscribe [:all-roles viz-id]) (rf/subscribe [:visible-roles viz-id])])
+    [(subscribe [:all-roles viz-id]) (subscribe [:visible-roles viz-id])])
   (fn [[all-roles visible-roles] _]
     (every? (set visible-roles) all-roles)))
